fb1ae54d755ffb16a79a2a72742ad4807a06df5d
= <Selenac99@hotmail.com>
2024-03-26 07:06:20 -0300

    aprendiendo a hacer una API con Express

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,18 +1,33 @@
+const http = require('node:http') // protocolo HTTP
+const fs = require('node:fs')
 
-  const http = require('http'); // No need to specify 'node:' in require
+const desiredPort = process.env.PORT ?? 1234
 
-  const desiredPort = process.env.PORT ?? 1234;
-  
-//abajo creamos un servidor con createServer, que recibe un callback (funciones que se ejecutan despues de que pase algo)
-  // Create a server
-  const server = http.createServer((req, res) => {
-    console.log('request received');
-    res.end('Hola mundo');
-  });
-  
-  //Escuchar el servidor en un puerto con otro callback
-  // Listen on the desired port
-  server.listen(desiredPort, () => {
-    console.log(`server listening on port http://localhost:${desiredPort}`);
-  });
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
+  if (req.url === '/') {
+    res.end('<h1>Mi página</h1>')
+  } else if (req.url === '/imagen-super-bonita.png') {
+    fs.readFile('./placa.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Server Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contacto') {
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404 // Not Found
+    res.end('<h1>404</h1>')
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
